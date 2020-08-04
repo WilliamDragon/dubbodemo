221df4a93d09dff423c7a280fe1b03bbe4b6537a
guojinong2 <15546601534@163.com>
2020-08-04 15:47:11 +0800

    更改测试信息

--- springboot-dubbo-server/src/main/java/com/gjl/parent/springbootdubboserver/DemoServiceImpl.java
+++ springboot-dubbo-server/src/main/java/com/gjl/parent/springbootdubboserver/DemoServiceImpl.java
@@ -3,13 +3,15 @@ package com.gjl.parent.springbootdubboserver;
 import com.gjl.parent.springvootdubboapi.DemoService;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DemoServiceImpl implements DemoService {
     @Override
     public void sayHello() {
-       /* System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + name + ", request from consumer: " + RpcContext.getContext().getRemoteAddress());
-        return "Hello " + name + ", response form provider: " + RpcContext.getContext().getLocalAddress();*/
-        System.out.println("hello,dubbo");
+        //System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + name + ", request from consumer: " + RpcContext.getContext().getRemoteAddress());
+        //return "Hello " + name + ", response form provider: " + RpcContext.getContext().getLocalAddress();
+        System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " );
+        System.out.println("hello,dubboto");
 
     }
 }
